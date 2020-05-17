266b2bba1999ef27f842de05c3b24d8247e54d60
Charlie Wren <cwren92@gmail.com>
2020-05-17 11:15:29 +0100

    Removing files

---
+++ PercolationStats.java
@@ -0,0 +1,29 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class PercolationStats {
+
+    // // perform independent trials on an n-by-n grid
+    // public PercolationStats(int n, int trials)
+    //
+    // // sample mean of percolation threshold
+    // public double mean()
+    //
+    // // sample standard deviation of percolation threshold
+    // public double stddev()
+    //
+    // // low endpoint of 95% confidence interval
+    // public double confidenceLo()
+    //
+    // // high endpoint of 95% confidence interval
+    // public double confidenceHi()
+    //
+    // test client
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
